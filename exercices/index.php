c9633542351da4c195e74fb852d3c9078508dc52
Harpogma <gd.temp@pm.me>
2025-03-19 14:25:25 +0100

    first commit

---
+++ exercices/index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Bienvenue dans les exercices de l'unité d'enseignement Programmation serveur 1 !<br>";
+
+//exercice 2
+$name = "Greg";
+echo "Hello $name<br>";
+
+//exercice 3
+$age = 19;
+if ($age >= 18) {
+    echo "You are over 18<br>";
+} else {
+    echo "You are under 18<br>";
+}
+
+//exercice 4
+const PI = 3.14;
+echo PI . "<br>";
