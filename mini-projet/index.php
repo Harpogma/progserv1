a5279d51a7d229ba5348aeb1c21a199a78c2d4aa
Harpogma <gd.temp@pm.me>
2025-03-23 21:32:23 +0100

    first step of the project is done

--- mini-projet/index.php
+++ mini-projet/index.php
@@ -1,2 +1,14 @@
 <?php
-echo "Bienvenue dans le mini-projet de l'unité d'enseignement Programmation serveur 1 !";
+require 'function.php';
+
+echo addPet("Caramel", 3);
+echo addPet("Rex", 8);
+echo addPet("Tweety", 1);
+echo addPet("Godzilla", 4);
+ 
+echo getPets();
+echo getPet("Rex");
+ 
+echo updatePet("Rex", 9);
+
+echo removePet("Tweety");
