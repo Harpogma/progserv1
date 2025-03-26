6b562678cd5def508e8bb08ae09f310225abd781
Harpogma <gd.temp@pm.me>
2025-03-26 09:15:55 +0100

    uncomment needed code

--- exercices_serie_2/exercice_026.php
+++ exercices_serie_2/exercice_026.php
@@ -1,9 +1,9 @@
 <?php
 
 function factorial($number) {
-    // if ($number == 0) {
-    //     return 1;
-    // }
+    if ($number == 0) {
+        return 1;
+    }
 
     return $number * factorial($number - 1);
 }
