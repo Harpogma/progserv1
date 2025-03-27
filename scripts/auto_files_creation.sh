5e259b3b0fca07ebd79931d0ee4b0c5ed47c7c83
Harpogma <gd.temp@pm.me>
2025-03-27 09:59:26 +0100

    Typo

--- scripts/auto_files_creation.sh
+++ scripts/auto_files_creation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 numberOfExercise=20 #change this value according to the number of files you want to create
-serieOfExercice=3 #change this value according to the serie of exercise (example: exercises_serie_1)
+serieOfExercise=3 #change this value according to the serie of exercise (example: exercises_serie_1)
 
 dir_name="exercises_serie_$serieOfExercice"
 mkdir -p "$dir_name" && cd "$dir_name" || exit 1
