e6a80a2806edb68674e383e2b4612e734398ffec
Harpogma <gd.temp@pm.me>
2025-03-30 20:38:29 +0200

    Change variable name to the english version. French version was causing a bug

--- scripts/auto_files_creation.sh
+++ scripts/auto_files_creation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-numberOfExercise=20 #change this value according to the number of files you want to create
+numberOfExercise=17 #change this value according to the number of files you want to create
 serieOfExercise=3 #change this value according to the serie of exercise (example: exercises_serie_1)
 
-dir_name="exercises_serie_$serieOfExercice"
+dir_name="exercices_serie_$serieOfExercise"
 mkdir -p "$dir_name" && cd "$dir_name" || exit 1
 
 for i in $(seq 1 $numberOfExercise); do
