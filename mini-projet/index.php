c612563431b3cd4a6d8ec7b79a6ef8364edb3e98
Harpogma <gd.temp@pm.me>
2025-03-31 21:30:27 +0200

    Recreated deleted project

---
+++ mini-projet/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'function.php';
+
+echo addPet("Caramel", 3);
+echo addPet("Rex", 8);
+echo addPet("Tweety", 1);
+echo addPet("Godzilla", 4);
+echo getPets();
+echo getPet("Rex");
+echo updatePet("Rex", 9);
+echo removePet("Tweety");
